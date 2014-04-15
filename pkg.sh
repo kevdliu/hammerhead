4e9dcebeaa02669e9a320b8a5651555bc4a87d05
kevdliu <kevdliu@gmail.com>
2014-04-15 17:30:57 -0400

    pkg script

---
+++ pkg.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+VER=$1
+
+if [ -z "$VER" ]; then
+    echo "No version provided"
+    exit 0
+fi
+
+# download boot image
+echo "Downloading boot image"
+cd ~/img-tools
+wget -N http://kernels.franco-lnx.net/Nexus5/4.4/boot-r$VER.img
+chmod 777 boot-r$VER.img
+
+# unpack boot image
+echo "Unpacking boot image"
+./unpackimg.sh boot-r$VER.img
+
+# replace zimage-dtb
+echo "Replacing kernel"
+cp -f ../ff/arch/arm/boot/zImage-dtb split_img/boot-r$VER.img-zImage
+
+# replace fstab.hammerhead
+echo "Replacing fstab"
+cp -f ../ff/fstab.hammerhead.f2fs ramdisk/fstab.hammerhead
+
+# repack boot image
+echo "Repacking boot image"
+./repackimg.sh
+
+# copy new boot image
+echo "Copying new boot image"
+cp -f image-new.img ../ff/arch/arm/boot/ff-f2fs-r$VER.img
+
+# clean up
+echo "Cleaning up"
+./cleanup.sh
+rm -f boot-r$VER.img
