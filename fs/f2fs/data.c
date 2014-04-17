1fa6cc71bbe1e2cd981147da8fab014e5874de76
kevdliu <kevdliu@gmail.com>
2014-04-17 15:41:44 -0400

    F2FS Update

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -790,8 +790,6 @@ static int f2fs_write_data_page(struct page *page,
 		.rw = (wbc->sync_mode == WB_SYNC_ALL) ? WRITE_SYNC : WRITE,
 	};
 
-	inode_dec_dirty_dents(inode);
-
 	if (page->index < end_index)
 		goto write;
 
@@ -800,8 +798,10 @@ static int f2fs_write_data_page(struct page *page,
 	 * this page does not have to be written to disk.
 	 */
 	offset = i_size & (PAGE_CACHE_SIZE - 1);
-	if ((page->index >= end_index + 1) || !offset)
+	if ((page->index >= end_index + 1) || !offset) {
+		inode_dec_dirty_dents(inode);
 		goto out;
+	}
 
 	zero_user_segment(page, offset, PAGE_CACHE_SIZE);
 write:
@@ -810,6 +810,7 @@ write:
 
 	/* Dentry blocks are controlled by checkpoint */
 	if (S_ISDIR(inode->i_mode)) {
+		inode_dec_dirty_dents(inode);
 		err = do_write_data_page(page, &fio);
 		goto done;
 	}
