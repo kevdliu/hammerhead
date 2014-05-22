86b461ced7023644f44a6705b6a1146a19900025
kevdliu <kevdliu@gmail.com>
2014-05-22 17:33:18 -0400

    Make zip

--- pkg.sh
+++ pkg.sh
@@ -33,7 +33,17 @@ echo "Repacking boot image"
 echo "Copying new boot image"
 cp -f image-new.img ../ff/arch/arm/boot/ff-f2fs-r$VER.img
 
+# make zip file
+echo "Making zip file"
+cd ../ff/zip
+rm -f *.zip
+cp -f ../arch/arm/boot/ff-f2fs-r$VER.img boot.img
+zip -r -9 ff-f2fs-r$VER.zip *
+mv ff-f2fs-r$VER.zip ../arch/arm/boot/ff-f2fs-r$VER.zip
+rm boot.img
+
 # clean up
 echo "Cleaning up"
+cd -
 ./cleanup.sh
 rm -f boot-r$VER.img
